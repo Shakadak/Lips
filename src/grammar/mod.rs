9fff6020dfdd622b665c5c2d24e610407d81c194
Shakadak <nathanael.pineau@gmail.com>
2015-02-04 16:50:42 +0100

    preparing tokenization, shitfting momentarily to simple prefix calculator

--- src/grammar/mod.rs
+++ src/grammar/mod.rs
@@ -1,4 +1,6 @@
-enum Operator
+use std::str::FromStr;
+
+pub enum Operator
 {
     Plus,
     Minus,
@@ -7,8 +9,36 @@ enum Operator
     Percent
 }
 
-enum Expression
+pub enum Token
 {
-    (Box<Expression>, Operator, Box<Expression>),
-    Literal
+    Operator(Operator),
+    Literal(isize)
+}
+
+pub enum Expression
+{
+    Literal(isize),
+    Expression
+        (
+            Operator,
+            Box<Expression>,
+            Box<Expression>
+        )
+}
+
+pub fn get_token(input: &str) -> Result<Token, &str>
+{
+    match input
+    {
+        "+"         =>  Ok(Token::Operator(Operator::Plus)),
+        "-"         =>  Ok(Token::Operator(Operator::Minus)),
+        "*"         =>  Ok(Token::Operator(Operator::Asterisk)),
+        "/"         =>  Ok(Token::Operator(Operator::Slash)),
+        "%"         =>  Ok(Token::Operator(Operator::Percent)),
+        input       =>  match FromStr::from_str(input)
+        {
+            Ok(number)      =>  Ok(Token::Literal(number)),
+            Err(_)          =>  Err(input)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate readline;
 
 use readline::{readline, add_history};
+use grammar::*;
+use std::result::Result;
+
+mod grammar;
 
 fn main()
 {
@@ -12,9 +16,19 @@ fn main()
             Ok(input)   =>
             {
                 add_history(input.as_slice());
-                println!("{}", input)
+                evaluate(input);
             },
             Err(_)      => break,
         }
     }
+}
+
+fn evaluate(input: String)
+{
+    let split = input
+        .split(|c: char| {c.is_whitespace()})
+        .map(get_token)
+        .filter(Result::is_ok);
+    let mut token = Vec::new();
+    for chunk in split {token.push(chunk.unwrap());}
 }
