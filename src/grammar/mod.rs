fd773bcd56e1535af61e17540fb2ecc6d64d84b5
nathanael <nathanael.pineau@gmail.com>
2015-07-10 20:59:38 +0200

    color test

--- src/grammar/mod.rs
+++ src/grammar/mod.rs
@@ -1,82 +1,52 @@
 use std::str::FromStr;
 use std::fmt;
 
-pub enum Operator
-{
+pub enum Operator {
     Plus,
     Minus,
     Asterisk,
     Slash,
-    Percent
-}
+    Percent }
 
-pub enum Token
-{
+pub enum Token {
     Operator(Operator),
-    Literal(isize)
-}
+    Literal(isize) }
 
-pub enum Expression
-{
+pub enum Expression {
     Literal(isize),
-    Expression
-        (
+    Expression (
             Operator,
             Box<Expression>,
-            Box<Expression>
-        )
-}
+            Box<Expression> )}
 
-impl fmt::Debug for Operator
-{
-    fn  fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
-    {
-        write!
-            (
+impl fmt::Debug for Operator {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
                 f,
                 "{}",
-                match *self
-                {
+                match *self {
                     Operator::Plus      =>  "+",
                     Operator::Minus     =>  "-",
                     Operator::Asterisk  =>  "*",
                     Operator::Slash     =>  "/",
-                    Operator::Percent   =>  "%"
-                }
-            )
-    }
-}
+                    Operator::Percent   =>  "%" })}}
 
-impl fmt::Debug for Token
-{
-    fn  fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
-    {
-        write!
-            (
+impl fmt::Debug for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
                 f,
                 "{}",
-                match *self
-                {
+                match *self {
                     Token::Operator(ref op)     =>  format!("Operator({:?})", op),
-                    Token::Literal(num)     =>  format!("Literal({})", num)
-                }
-            )
-    }
-}
+                    Token::Literal(num)     =>  format!("Literal({})", num) })}}
 
-pub fn assign_token(input: &str) -> Result<Token, String>
-{
-    match input
-    {
+pub fn assign_token(input: &str) -> Result<Token, String> {
+    match input {
         "+"         =>  Ok(Token::Operator(Operator::Plus)),
         "-"         =>  Ok(Token::Operator(Operator::Minus)),
         "*"         =>  Ok(Token::Operator(Operator::Asterisk)),
         "/"         =>  Ok(Token::Operator(Operator::Slash)),
         "%"         =>  Ok(Token::Operator(Operator::Percent)),
-        input       =>  match FromStr::from_str(input)
-        {
+        input       =>  match FromStr::from_str(input) {
             Ok(number)      =>  Ok(Token::Literal(number)),
-            Err(_)          =>  Err(format!("Error: unassignable token ({})", input))
-        }
-    }
-}
+            Err(_)          =>  Err(format!("Error: unassignable token ({})", input)) }}}
