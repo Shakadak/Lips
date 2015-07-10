10689ba4dd7fa95520ab41b63aa16078d77d79a1
nathanael <nathanael.pineau@gmail.com>
2015-07-10 05:03:48 +0200

    color test

--- src/main.rs
+++ src/main.rs
@@ -6,44 +6,28 @@ use std::result::Result;
 
 mod grammar;
 
-fn main()
-{
-    loop
-    {
+fn main() {
+    loop {
         let result = readline("Kiss! ");
-        match result
-        {
-            Some(input)     =>
-            {
-                add_history(input.as_slice());
-                for token in tokenize(input)
-                {
-                    match token
-                    {
-                        Ok(token)   =>
-                        {
-                            println!("      {:?}", token)
-                        }
-                        Err(error)   =>
-                        {
-                            println!("      {}", error);
-                            break
-                        },
-                    }
-                }
-            },
-            None            =>
-            {
-                break
-            },
-        }
-    }
-}
+        match result {
+            Some(input)
+                => {
+                    add_history(input.as_slice());
+                    for token in tokenize(input) {
+                        match token {
+                            Ok(token)
+                                => {
+                                    println!("      {:?}", token) }
+                            Err(error)
+                                => {
+                                    println!("      {}", error);
+                                    break },}}}
+            None
+                => {
+                    break }}}}
 
-fn tokenize(input: String) -> Vec<Result<Token, String>>
-{
+fn tokenize(input: String) -> Vec<Result<Token, String>> {
     input
         .split(CharExt::is_whitespace)
         .map(assign_token)
-        .collect()
-}
+        .collect() }
